1d72e02a9fdecd5f54be189a60eb3479affd97f9
Alexandre Insua Moreira <alexandreinsua@gmail.com>
2026-02-09 19:36:43 +0100

    chore(project): adjusts server

--- server/server.ts
+++ server/server.ts
@@ -1,23 +1,22 @@
-
 import * as express from 'express';
-import {Application} from "express";
-import {getAllCourses, getCourseById} from "./get-courses.route";
-import {searchLessons} from "./search-lessons.route";
-import {saveCourse} from './save-course.route';
-import {loginUser} from './login.route';
-import {createCourse} from "./create-course.route";
-import {deleteCourse} from "./delete-course.route";
-import {saveLesson} from "./save-lesson.route";
+import { Application } from 'express';
+import { createCourse } from './create-course.route';
+import { deleteCourse } from './delete-course.route';
+import { getAllCourses, getCourseById } from './get-courses.route';
+import { loginUser } from './login.route';
+import { saveCourse } from './save-course.route';
+import { saveLesson } from './save-lesson.route';
+import { searchLessons } from './search-lessons.route';
 
 const bodyParser = require('body-parser');
 
-const app: Application = express();
+const app: Application = express.default();
 
 app.use(bodyParser.json());
 
 const cors = require('cors');
 
-app.use(cors({origin: true}));
+app.use(cors({ origin: true }));
 
 app.route('/api/courses').get(getAllCourses);
 
@@ -36,5 +35,7 @@ app.route('/api/lessons/:id').put(saveLesson);
 app.route('/api/login').post(loginUser);
 
 const httpServer = app.listen(9000, () => {
-  console.log("HTTP REST API Server running at http://localhost:" + httpServer.address()["port"]);
+  const address = httpServer.address();
+  const port = typeof address === 'object' ? address?.port : 9000;
+  console.log('HTTP REST API Server running at http://localhost:' + port);
 });

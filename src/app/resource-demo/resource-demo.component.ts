5201773208b0fd2f2521e48b94c48e5ae3774bc2
Alexandre Insua Moreira <alexandreinsua@gmail.com>
2026-03-09 18:18:29 +0100

    feature: implements lessons feature

--- src/app/resource-demo/resource-demo.component.ts
+++ src/app/resource-demo/resource-demo.component.ts
@@ -1,39 +1,41 @@
-import {Component, effect, inject, resource, signal} from "@angular/core";
-import {MatProgressSpinner} from "@angular/material/progress-spinner";
-import {environment} from "../../environments/environment";
-import {Lesson} from "../models/lesson.model";
-
+import { Component, effect, resource, signal } from '@angular/core';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { environment } from '../../environments/environment.development';
+import { Lesson } from '../models/lesson.model';
 
 @Component({
   selector: 'resource-demo',
   templateUrl: './resource-demo.component.html',
   styleUrls: ['./resource-demo.component.scss'],
-  imports: [MatProgressSpinner]
+  imports: [MatProgressSpinner],
 })
 export class ResourceDemoComponent {
-
   env = environment;
 
   search = signal<string>('');
 
-  lessons = signal<Lesson[]>([])
+  lessons = resource<Lesson[], { query: string }>({
+    params: () => ({ query: this.search() }),
+    loader: async (params) => {
+      const response = await fetch(
+        `${this.env.apiRoot}/search-lessons?query=${params.params.query}&courseId=18`,
+      );
+      const data = await response.json();
+      return data.payload;
+    },
+  });
 
   constructor() {
-
     effect(() => {
-      console.log('searching lessons:', this.search() );
-    })
+      console.log('searching lessons:', this.search());
+    });
   }
 
   searchLessons(search: string) {
     this.search.set(search);
   }
 
-  reset() {
+  reset() {}
 
-  }
-
-  reload() {
-
-  }
+  reload() {}
 }
